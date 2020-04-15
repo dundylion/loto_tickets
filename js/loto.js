12132863e39a4c1b7d2e5907fa874534230d58c5
dundylion <silent_84@mail.ru>
2020-04-16 03:17:38 +0700

    Add main functionality, add cards.js with 6 cards Need to correct randomizer. Reset activated status after press "another" button

--- js/loto.js
+++ js/loto.js
@@ -1,6 +1,9 @@
 function init() {
 	$("#fplus").click(plusField);
 	$("#fminus").click(minusField);
+	$(".changebutton").click(fillCard);
+	$(".changebutton").click();
+	$(".cell").click(activateCell);
 }
 
 function plusField() {
@@ -12,3 +15,31 @@ function minusField() {
 	// console.log(b.length);
 	if (b.length>1) {b.last().toggleClass('hidden');}
 	};
+
+function fillCard(crd) {
+	let card = crd;
+	let cells = card.target.parentElement.querySelectorAll(".cell");
+	ks = Object.keys(cards);
+	let cr = Math.floor(Math.random()*ks.length);
+	crd.target.previousElementSibling.innerText = ks[cr];
+	let numbers = cards[ks[cr]];
+	for (let i = 0; i<cells.length; i++)
+		{
+			t = cards[ks[cr]][i];
+			cells[i].innerText = t;
+		}
+	// $(card)(".activated").target.classList.remove("activated");
+	}
+function activateCell(cell) {
+	console.log(cell);
+	if (cell.target.innerText !== "") {
+		cell.target.classList.toggle("activated");
+		checkRow(cell.target.parentElement);
+	}	
+}
+function checkRow(row) {
+	l = row.querySelectorAll(".activated").length;
+	if (l == 5) {row.classList.add("activated")}
+		else {row.classList.remove("activated")}
+
+}
